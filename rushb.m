59e2beae4f6b584b80fef2d2bc2e6e1cae039e13
Aryan Keluskar <aryankeluskar6@gmail.com>
2023-11-09 11:54:52 +0530

    milestone 2

--- rushb.m
+++ rushb.m
@@ -13,11 +13,30 @@ while (1)
         brick.StopMotor('AB');
         pause(1);
     end
+    
+    color = brick.ColorCode(3);
+    if color == 5 % Green
+        brick.MoveMotor('A', 20);
+        pause(1);
+        brick.StopMotor('A');
+    elseif color == 2 % Blue
+        brick.MoveMotor('B', 20);
+        pause(1);
+        brick.StopMotor('B');
+    elseif color == 3 % Yellow
+        brick.MoveMotor('AB', 20);
+        pause(1);
+        brick.StopMotor('AB');
+    elseif color == 1 % Black
+        brick.MoveMotor('AB', -20);
+        pause(1);
+        brick.StopMotor('AB');
+    end
+    
     brick.MoveMotor('AB', -10);
     pause(0.5*5);
     brick.StopAllMotors();
-
-
+    
     % display(brick.UltrasonicDist(4))
     % brick.MoveMotor('AB', -100);
     % pause(0.5);
